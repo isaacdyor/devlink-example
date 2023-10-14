a486c1496c743276188774dab26e9f090cfd941a
Isaac Dyor <isaac@dyor.com>
2023-10-14 11:30:21 -0700

    adding counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import type { NextPage } from "next/types";
+import React, { useState } from "react";
+import { NextPage } from "next/types";
 
 const ExamplePage: NextPage = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increase Count</button>
+    </div>
+  );
 };
 
 export default ExamplePage;
